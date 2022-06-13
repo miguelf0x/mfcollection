1527acba3f36c10fa45ee9df25d13708485a753e
Miguel <foxxer2602@gmail.com>
2022-06-13 03:40:18 +0300

    90% of script rewritten

--- mfcollection.sh
+++ mfcollection.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 ############################################################
-# Vars                                                     #
+# Variables                                                #
 ############################################################
 
-Verbose=true
+Verbose=1
+Tweaking=1
 
 ############################################################
 # Help                                                     #
@@ -14,11 +15,13 @@ Help()
 {
    # Display Help
    echo "This script is designed for automatisation of installing some necessary packages"
+   echo "Script should not be run as root"
    echo
-   echo "Syntax: mfcollection [-v|-h]"
+   echo "Syntax: mfcollection [-v|-h|-t]"
    echo "Options:"
    echo "h     Print this help."
    echo "v     Verbose mode."
+   echo "t     Applies minor tweaks to system"
    echo
 }
 
@@ -32,158 +35,116 @@ IsPkgInstalled()
 }
 
 ############################################################
-# InstallIfNotExist                                        #
-############################################################
-
-InstallIfNotExist()
-{
-   local package=$packageName
-   echo $package
-   if sudo pacman -Qs "$packageName" > /dev/null
-   then
-     if $Verbose
-     then
-       echo "Package $package is already installed!"
-     fi
-   else
-     if $Verbose
-     then
-       sudo pacman -Sq "$package" --noconfirm  --noprogressbar
-     else
-       sudo pacman -Sq "$package" --noconfirm  --noprogressbar > /dev/null
-     fi
-   fi
-}
-
-
-############################################################
 # Main code                                                #
 ############################################################
 
-while getopts ":hv:" option; do
+while getopts ":hvt:" option; do
    case $option in
-      h) # Show help
+      h) # display Help
          Help
          exit;;
       v) # Verbose mode
-         Verbose=false;;
+         Verbose=0;;
+      t) # Tweaking mode
+          Verbose=0;;
      \?) # Invalid option
          echo "Error: Invalid option"
          exit;;
    esac
 done
 
-if $Verbose
-then
-  echo "1. Updating system"
-  sudo pacman -Syyu --noconfirm --noprogressbar
-  echo "2. Removing unneeded packages"
-  if sudo pacman -Qdtq
-  then
-    sudo pacman -Rns $(pacman -Qdtq) --noconfirm  --noprogressbar
-  fi
-else
-  sudo pacman -Syyu --noconfirm --noprogressbar > /dev/null
-  if sudo pacman -Qdtq > /dev/null
-  then
-    sudo pacman -Rns $(pacman -Qdtq) --noconfirm  --noprogressbar > /dev/null
-  fi
-fi
+case $Verbose in
+    1) # Verbose mode is OFF
+        sudo pacman -Syyu --noconfirm --noprogressbar > /dev/null
+        sudo pacman -Rns $(pacman -Qdtq) --noconfirm  --noprogressbar > /dev/null
+        if IsPkgInstalled git
+        then
+          sudo pacman -S git --noconfirm  --noprogressbar > /dev/null
+        fi
+        if IsPkgInstalled curl
+        then
+          sudo pacman -S curl --noconfirm  --noprogressbar > /dev/null
+        fi
+        if IsPkgInstalled base-devel
+        then
+          sudo pacman -S base-devel --noconfirm  --noprogressbar > /dev/null
+        fi
+        cd /tmp
+        git clone https://aur.archlinux.org/yay.git > /dev/null
+        sudo chown -R $USER yay > /dev/null
+        cd yay
+        makepkg -si --noconfirm --noprogressbar > /dev/null
+        ;;
+    0) # Verbose mode is ON
+        echo "1. Updating system"
+        sudo pacman -Syyu --noconfirm --noprogressbar
+        echo "2. Removing unrequired packages"
+        sudo pacman -Rns $(pacman -Qdtq) --noconfirm  --noprogressbar
+        echo "3. Installing basic development tools"
+        if IsPkgInstalled git
+        then
+          sudo pacman -S git --noconfirm  --noprogressbar
+        fi
+        if IsPkgInstalled curl
+        then
+          sudo pacman -S curl --noconfirm  --noprogressbar
+        fi
+        if IsPkgInstalled base-devel
+        then
+          sudo pacman -S base-devel --noconfirm  --noprogressbar
+        fi
+        echo "4. Installing yay"
+        cd /tmp
+        git clone https://aur.archlinux.org/yay.git
+        sudo chown -R $USER yay
+        cd yay
+        makepkg -si --noconfirm --noprogressbar
+        ;;
+esac
 
-InstallIfNotExist git
-InstallIfNotExist curl
-InstallIfNotExist base-devel
-InstallIfNotExist atop
-
-#if Verbose=1
-#then
-#  echo "3. Installing basic development tools"
-#  if IsPkgInstalled git
-#  then
-#    sudo pacman -S git --noconfirm  --noprogressbar
-#  fi
-#  if IsPkgInstalled curl
-#  then
-#    sudo pacman -S curl --noconfirm  --noprogressbar
-#  fi
-#  if IsPkgInstalled base-devel
-#  then
-#    sudo pacman -Sq base-devel --noconfirm  --noprogressbar
-#  fi
-#else
-#  if IsPkgInstalled git
-#  then
-#    sudo pacman -S git --noconfirm  --noprogressbar > /dev/null
-#  fi
-#  if IsPkgInstalled curl
-#  then
-#    sudo pacman -S curl --noconfirm  --noprogressbar > /dev/null
-#  fi
-#  if IsPkgInstalled base-devel
-#  then
-#    sudo pacman -S base-devel --noconfirm  --noprogressbar > /dev/null
-#  fi
-
-cd /tmp
-git clone https://aur.archlinux.org/yay.git
-sudo chown -R $USER yay
-cd yay
-makepkg -si --noconfirm --noprogressbar
-
-if $Verbose
-then
-  echo "4. Installing optional packages"
-fi
-
-#Office tools
-
-if IsPkgInstalled nano
-then
-  echo "Install nano CLI text editor?"
-  read -r -p "Your choice [y/N] " response
-  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-  then
-      sudo pacman -S nano --noconfirm --noprogressbar
-  else
-      :
-  fi
-fi
-
-if IsPkgInstalled atom
-then
-  echo "Install Atom GUI text editor?"
-  read -r -p "Your choice [y/N] " response
-  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-  then
-      sudo pacman -S atom --noconfirm --noprogressbar
-  else
-      :
-  fi
-fi
-
-#Internet tools
-#pacman -S firefox --noconfirm
-
-echo "Install file transfer tools? (qbittorrent, filezilla)"
-read -r -p "Your choice [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    sudo pacman -S qbittorrent filezilla --needed --noconfirm --noprogressbar
-else
-    :
-fi
-
-echo "Install messengers? (telegram, discord)"
-read -r -p "Your choice [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    sudo pacman -S telegram-desktop discord --needed --noconfirm --noprogressbar
-else
-    :
-fi
-
-#Multimedia
-#pacman -S smplayer --noconfirm
-
-#Development tools
-#pacman -S tigervnc --noconfirm
+case $Tweaking in
+    1) #Tweaking is disabled
+        ;;
+    0) #Tweaking is enabled
+        cd /tmp
+        echo "Enable SysRq key?"
+        read -r -p "Your choice [y/N] " response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+            echo "kernel.sysrq=1" >> 90-override.conf.tmp
+        else
+            :
+        fi
+        echo "Change vm.swappiness to optimum value?"
+        read -r -p "Your choice [y/N] " response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+            mem=$(awk '/MemFree/ { printf "%.3f \n", $2/1024/1024 }' /proc/meminfo)
+            if (($mem<=512))
+            then
+                $swappiness_opt = 60
+            elif ((512<$mem && $mem<=2048))
+            then
+                $swappiness_opt = 50
+            elif ((2048<$mem && $mem<=8192))
+            then
+                $swappiness_opt = 40
+            elif ((8192<$mem && $mem<=16384))
+            then
+                $swappiness_opt = 20
+            else
+                $swappiness_opt = 10
+            fi
+            echo "vm.swappiness = $swappiness_opt" >> 90-override.conf.tmp
+        else
+            :
+        fi
+        echo "Are these options correct?"
+        echo "##############################"
+        cat 90-override.conf.tmp
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+            cp 90-override.conf.tmp /etc/sysctl.d/90-override.conf
+        else
+            :
+        fi
